1416290fe46b0114b49887bb6b575c14ef6134b9
Patrick Snape <pts08@ic.ac.uk>
2012-01-26 14:32:56 +0000

    Main script now calculates mean surface normal

--- scripts/generateTrainingSet.m
+++ scripts/generateTrainingSet.m
@@ -17,6 +17,7 @@ training_set.shapes = generateRandomShapes(base_morphable_model);
 %% Generate normals
 
 training_set.normals = generateShapeNormals(training_set.shapes, base_morphable_model.tl);
+training_set.mean_surface_norm = mean_surface_norm(training_set.normals);
 
 disp('Training Set Successfully Created');
 
