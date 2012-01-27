acf46295a9905d729cd0d387934c74273c489dd8
Patrick Snape <pts08@ic.ac.uk>
2012-01-27 14:30:25 +0000

    Add function to visualize normals of face in training set

---
+++ functions/visualize.m
@@ -0,0 +1,18 @@
+function visualize( training_set, tl, index )
+%VISUALIZE_FIRST Visualizes the face at the specified index
+%   Assumes plot_mesh is available
+
+if (index > size(training_set, 2))
+    error('Index is out of range of training set')
+end
+
+shape = reshape(training_set.shapes(:, index), [ 3 numel(training_set.shapes(:, index))/3 ]);
+normals = reshape(training_set.normals(:, index), [ 3 numel(training_set.normals(:, index))/3 ]);
+
+options = [];
+options.normal = normals;
+
+plot_mesh(shape, tl, options);
+
+end
+
