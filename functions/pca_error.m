c274a1b069e58326c44b9771173e9cfa6c1b683e
Patrick Snape <pts08@ic.ac.uk>
2012-01-27 14:39:04 +0000

    Function to calculate the normalized error in performing PCA

---
+++ functions/pca_error.m
@@ -0,0 +1,15 @@
+function err = pca_error( X, Xavg )
+%PCA_ERROR Calculates the normalized error of computing the principal
+%components
+
+normalized_normals = matsubcolvec(X, Xavg);
+
+U = myGPCA(normalized_normals, 199, 0);
+
+Y = U' * normalized_normals;
+Xtilde = mataddcolvec(U * Y, Xavg);
+
+err = norm(Xtilde - X, 2);
+
+end
+
