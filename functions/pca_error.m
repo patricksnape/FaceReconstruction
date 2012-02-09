1ada769fcfaf2a1dbf113f4ee94752766d2bdb0d
Patrick Snape <pts08@ic.ac.uk>
2012-02-09 14:14:32 +0000

    Don't use hardcoded 199 - calculating using number of columns

--- functions/pca_error.m
+++ functions/pca_error.m
@@ -4,7 +4,7 @@ function err = pca_error( X, Xavg )
 
 normalized_normals = matsubcolvec(X, Xavg);
 
-U = myGPCA(normalized_normals, 199, 0);
+U = myGPCA(normalized_normals, size(X, 2) - 1, 0);
 
 Y = U' * normalized_normals;
 Xtilde = mataddcolvec(U * Y, Xavg);
