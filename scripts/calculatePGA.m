1e6814fa5aa5ca2af419d1a8a5cf94d8ef8ea522
Patrick Snape <pts08@ic.ac.uk>
2012-02-09 14:15:38 +0000

    Updated calculatePGA to use textprogressbar and to use a subset of the normals (and to calculate the mean of that subset)

--- scripts/calculatePGA.m
+++ scripts/calculatePGA.m
@@ -1,21 +1,23 @@
 %% Assumes generateTrainingSet has been run. Calculates all spherical medians.
 
-N = size(training_set.normals, 1) / 3;
+% Allows me to use a subset of the training_set
+normals_set = training_set.normals(:, 1:3);
+mean_normals_set = mean_surface_norm(normals_set);
+
+N = size(normals_set, 1) / 3;
 mus = zeros(3, N);
 
-disp('Generating intrinsic means for each normal...');
-
+textprogressbar('Generating intrinsic means for each normal: ');
 for i = 1:3:N
-    normalp = [training_set.normals(i, :); training_set.normals(i+1, :); training_set.normals(i+2, :)];
+    normalp = [normals_set(i, :); normals_set(i+1, :); normals_set(i+2, :)];
     % column vector
     normalp = reshape(normalp, [], 1);
     mu = sphericalmedian(normalp);
 
     mus(:, ceil(i / 3)) = mu;  
-    progressbar(i, N);
+    textprogressbar((ceil((i/N)*100)));
 end
-disp(' ');
-disp('Finished generating intrinsic means for each normal.');
+textprogressbar(' done');
 
 % column vector
 %mus = reshape(mus, [], 1);
@@ -26,11 +28,11 @@ clear mu;
 
 %% Calculate D
 
-K = size(training_set.normals, 2);
+K = size(normals_set, 2);
 D = zeros(N * 3, K);
-normals = reshape(training_set.normals, [3 numel(training_set.normals)/3]);
+normals = reshape(normals_set, [3 numel(normals_set)/3]);
 
-disp('Generating D...');
+textprogressbar('Generating D: ');
 % for each face
 for i = 1:K
     vk = zeros(3, N);
@@ -39,10 +41,9 @@ for i = 1:K
         vk(:, k) = logmap(mus(:, k), normals(:, k));
     end
     D(:, i) = reshape(vk, [], 1);
-    progressbar(i, K);
+    textprogressbar(((i/K)*100));
 end
-disp(' ');
-disp('Finished generating D.');
+textprogressbar(' done');
 
 clear normals;
 clear vk;
@@ -51,7 +52,7 @@ clear vk;
 
 disp('Generating DAvg...');
 DAvg = zeros(N * 3, 1);
-normals = reshape(training_set.mean_surface_norm, [3 numel(training_set.mean_surface_norm)/3]);
+normals = reshape(mean_normals_set, [3 numel(mean_normals_set)/3]);
 vk = zeros(3, N);
 
 % for all normals
@@ -59,7 +60,7 @@ parfor k = 1:N
     vk(:, k) = logmap(mus(:, k), normals(:, k));
 end
 DAvg(:, 1) = reshape(vk, [], 1);
-disp('Finished generating DAvg...');
+disp('Finished generating DAvg.');
 
 clear normals;
 clear vk;
