b2d6e2809b1ed0f886c3690d51cd52005a749558
Patrick Snape <pts08@ic.ac.uk>
2012-02-09 19:04:03 +0000

    Use fminsearch instead of custom loop

--- functions/sphericalmedian.m
+++ functions/sphericalmedian.m
@@ -1,4 +1,4 @@
-function [mu] = sphericalmedian(normals)
+function [mu] = sphericalmedian(guess, normals)
 %SPHERICALMEAN Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,26 +6,23 @@ function [mu] = sphericalmedian(normals)
 vec_normals = reshape(normals, [3 numel(normals)/3]);
 
 N = size(vec_normals, 2);
-mu = vec_normals(:, 1);
+mu = guess;
 delta_mu = [0;0;0];
 
 tol = 10 ^ -3;
 not_done = true;
 
-while not_done
-    prev_mu = mu;
-    
-    for i = 1:N
-        l = logmap(mu, vec_normals(:, i));
-        if (~any(isnan(l)))
-            delta_mu = delta_mu + l;
-        end
+prev_mu = mu;
+
+for i = 1:N
+    l = logmap(mu, vec_normals(:, i));
+    if (~any(isnan(l)))
+        delta_mu = delta_mu + l;
     end
-    delta_mu = delta_mu / N;
-    
-    mu = expmap(mu, delta_mu);
-    not_done = (norm(mu-prev_mu) > tol);
 end
+delta_mu = delta_mu / N;
+
+mu = expmap(mu, delta_mu);
+mu = norm(mu-prev_mu);
 
 end
-
