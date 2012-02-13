112d97eb4627026856b0100dfa676333efabaaaa
Patrick Snape <pts08@ic.ac.uk>
2012-02-13 20:44:45 +0000

    Take the definition of the intrinsic mean from Smith's paper

--- functions/sphericalmedian.m
+++ functions/sphericalmedian.m
@@ -1,4 +1,4 @@
-function [mu] = sphericalmedian(guess, normals)
+function [delta_mu] = sphericalmedian(guess, normals)
 %SPHERICALMEAN Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,23 +6,12 @@ function [mu] = sphericalmedian(guess, normals)
 vec_normals = reshape(normals, [3 numel(normals)/3]);
 
 N = size(vec_normals, 2);
-mu = guess;
-delta_mu = [0;0;0];
-
-tol = 10 ^ -3;
-not_done = true;
-
-prev_mu = mu;
+delta_mu = 0;
 
 for i = 1:N
-    l = logmap(mu, vec_normals(:, i));
-    if (~any(isnan(l)))
-        delta_mu = delta_mu + l;
-    end
+    l = logmap(guess, vec_normals(:, i));
+    delta_mu = delta_mu + norm(l);
 end
 delta_mu = delta_mu / N;
-
-mu = expmap(mu, delta_mu);
-mu = norm(mu-prev_mu);
 
 end
