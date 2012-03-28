d34ffe8ef0e933bab5cef0f9dd9b6bad8fd4d51f
Patrick Snape <pts08@ic.ac.uk>
2012-03-28 15:18:17 +0100

    Added script to align new training set

---
+++ scripts/AlignTrainingSet.m
@@ -0,0 +1,14 @@
+%% Assumes I_model exists
+
+for i=1:199
+    shape = I_model{i}.shape;
+    I_model{i}.aligned = AlignFace(I_model{i}.normBuffer, ...
+                                   shape(1,2), ... % Nose Y
+                                   shape(2,2), ... % Left Eye Y
+                                   shape(3,2), ... % Chin Y
+                                   shape(4,2), ... % Right Eye Y
+                                   shape(1,1), ... % Nose X
+                                   shape(2,1), ... % Left Eye X
+                                   shape(3,1), ... % Chin X
+                                   shape(4,1));    % Right Eye X
+end
