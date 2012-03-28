f0a6103ed789db2015ef671650f8633ceee850f5
Patrick Snape <pts08@ic.ac.uk>
2012-03-28 15:06:27 +0100

    Added overload to method to accept single image

--- functions/Image2ColVector3.m
+++ functions/Image2ColVector3.m
@@ -1,12 +1,19 @@
-function vec = Image2ColVector3(x, y, z)
+function vec = Image2ColVector3(xs, ys, zs)
 %IMAGE2COLVECTOR Convert to 3 x N matrix by concatenating the rows of an
 %image then convert to column vector
 
-% convert to 3xN matrix by concatenating the rows of an image
-x = reshape(x ,1, []);
-y = reshape(y, 1, []);
-z = reshape(z, 1, []);
+% Allow a single NxMx3 image to be passed in (method overload)
+if (ndims(xs) == 3)
+    x = reshape(xs(:,:,1), 1, []);
+    y = reshape(xs(:,:,2), 1, []);
+    z = reshape(xs(:,:,3), 1, []);
+else
+    x = reshape(xs ,1, []);
+    y = reshape(ys, 1, []);
+    z = reshape(zs, 1, []);
+end
 
+% convert to 3xN matrix by concatenating the rows of an image
 vec = [x;y;z];
 
 % convert 3xN rows to a column matrix
