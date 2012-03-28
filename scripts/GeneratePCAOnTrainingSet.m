3c7083ffc5e8f44451bf633162918e874245425a
Patrick Snape <pts08@ic.ac.uk>
2012-03-28 19:12:22 +0100

    Added new script to generate PCA

---
+++ scripts/GeneratePCAOnTrainingSet.m
@@ -0,0 +1,13 @@
+%% Assumes I_model exists
+
+X = cellfun(@(x) getfield(x, 'aligned'), I_model, 'UniformOutput', false);
+X = cellfun(@(x) Image2ColVector3(x), X, 'UniformOutput', false);
+X = cell2mat(X');
+
+Xavg = sum(X, 2);
+Xavg = Xavg/199;
+
+Xnorm = X - repmat(Xavg, 1, 199);
+pcaerror = pca_error(X, Xavg);
+
+U = myGPCA(Xnorm, 199, 0);
