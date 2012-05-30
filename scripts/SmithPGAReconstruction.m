5317f949c7bd2a6a1613cc3edfcaa8e823ba0519
Patrick Snape <pts08@ic.ac.uk>
2012-05-30 23:31:23 +0100

    Rename n in pga script to npga

--- scripts/SmithPGAReconstruction.m
+++ scripts/SmithPGAReconstruction.m
@@ -3,5 +3,5 @@ load('data/pga')
 load('data/lights')
 light = lights(:,1) / colnorm(lights(:,1));
 AlignMut1
-[ b n ] = SmithPGAGSS(rgb2gray(mutaligned), UPGA, mean_normals_set, mus, light');
-TexturizeRecoveredFace(rgb2gray(mutaligned), n);
+[ b npga ] = SmithPGAGSS(rgb2gray(mutaligned), UPGA, mean_normals_set, mus, light');
+TexturizeRecoveredFace(rgb2gray(mutaligned), npga);
