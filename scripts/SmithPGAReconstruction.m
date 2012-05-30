4a60e530e971a52f1fa43b1e6af32d8be0d218fc
Patrick Snape <pts08@ic.ac.uk>
2012-05-30 23:30:38 +0100

    Don't recompute the PGA every time just load it

--- scripts/SmithPGAReconstruction.m
+++ scripts/SmithPGAReconstruction.m
@@ -1,7 +1,7 @@
 load('data/trainingset')
+load('data/pga')
 load('data/lights')
 light = lights(:,1) / colnorm(lights(:,1));
 AlignMut1
-calculatePGA
 [ b n ] = SmithPGAGSS(rgb2gray(mutaligned), UPGA, mean_normals_set, mus, light');
 TexturizeRecoveredFace(rgb2gray(mutaligned), n);
