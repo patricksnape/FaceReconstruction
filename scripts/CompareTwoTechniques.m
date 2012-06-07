f185361ddf45dc1208e857010886a14f5f6d492e
Patrick Snape <pts08@ic.ac.uk>
2012-06-07 12:58:30 +0100

    changed compare techniques to generate all
    Changed some variable names and added AEP

--- scripts/CompareTwoTechniques.m
+++ scripts/CompareTwoTechniques.m
@@ -1,28 +1,38 @@
 load('data/trainingset')
-light = [0;0;1];
+load('data/pga');
+load('data/lights');
+light = lights(:,1) / colnorm(lights(:,1));
+%light = [0;0;1];
+texture = rgb2gray(mutaligned);
 GeneratePCAOnTrainingSet
-calculatePGA
+GenerateAEPPCAOnTrainingSet
 
 figure(1);
 clf;
-[error b n] = SmithGSS(I_model{1}.alignedTexture, Un, XNavg, light');
-TexturizeRecoveredFace(I_model{1}.alignedTexture, n);
+[error b n] = SmithGSS(texture, Un, XNavg, light');
+TexturizeRecoveredFace(texture, n);
 
 figure(2);
 clf;
-[ a c ] = NewReconstruction(I_model{1}.alignedTexture,Un, Ut, light);
-[nestimate testimate] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, c, a);
-TexturizeRecoveredFace(I_model{1}.alignedTexture, nestimate);
+[b naep] = SmithAEPGSS(texture, UAEPn, XNavg, light');
+TexturizeRecoveredFace(texture, naep);
 
 figure(3);
 clf;
-[ b npga ] = SmithPGAGSS(I_model{1}.alignedTexture, UPGA, mean_normals_set, mus, light');
-TexturizeRecoveredFace(I_model{1}.alignedTexture, npga);
+[ a c ] = NewReconstruction(texture,Un, Ut, light);
+[nestimate testimate] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, c, a);
+TexturizeRecoveredFace(texture, nestimate);
 
 figure(4);
 clf;
-TexturizeRecoveredFace(I_model{1}.alignedTexture, I_model{1}.alignedNormals);
+[ b npga ] = SmithPGAGSS(texture, UPGA, mean_normals_set, mus, light');
+TexturizeRecoveredFace(texture, npga);
+
+figure(5);
+clf;
+TexturizeRecoveredFace(texture, I_model{1}.alignedNormals);
 
 smithError = AngularError(I_model{1}.alignedNormals, n)
+AEPError = AngularError(I_model{1}.alignedNormals, naep)
 PGAError = AngularError(I_model{1}.alignedNormals, npga)
 myError = AngularError(I_model{1}.alignedNormals, nestimate)
