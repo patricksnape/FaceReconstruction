b128951439c8925cfa045d6855d1c2bdc5b34303
Patrick Snape <pts08@ic.ac.uk>
2012-06-12 14:45:29 +0100

    Script to align bln

---
+++ scripts/AlignBln1.m
@@ -0,0 +1,8 @@
+bln1 = imread('data/images/bln1.ppm');
+blnaligned1 = AlignFace(bln1, 325, 263, 262, 452, 377, 299, 450, 379);
+bln2 = imread('data/images/bln2.ppm');
+blnaligned2 = AlignFace(bln2, 325, 263, 262, 452, 377, 299, 450, 379);
+bln3 = imread('data/images/bln3.ppm');
+blnaligned3 = AlignFace(bln3, 325, 263, 262, 452, 377, 299, 450, 379);
+bln4 = imread('data/images/bln4.ppm');
+blnaligned4 = AlignFace(bln4, 325, 263, 262, 452, 377, 299, 450, 379);
