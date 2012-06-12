24738adfb2fba8916f391e7a3fc7a1411b56f753
Patrick Snape <pts08@ic.ac.uk>
2012-06-12 14:44:20 +0100

    Compute an angular error image

--- functions/AngularError.m
+++ functions/AngularError.m
@@ -1,14 +1,19 @@
-function [ error ] = AngularError(groundtruth, estimate)
+function [ error ] = AngularError(groundtruth, nestimate)
 %ANGULARERROR Calculates the sum of the absolute difference in angle between
 %the two given normals fields
 
+    [m n ~] = size(nestimate);
     groundtruth = reshape2colvector(Image2ColVector3(groundtruth));
-    estimate = reshape2colvector(Image2ColVector3(estimate));
+    estimate = reshape2colvector(Image2ColVector3(nestimate));
 
-    error = 0;
+    error = zeros(size(estimate, 2), 1);
     
     for i = 1:size(estimate, 2)
-        error = error + abs(atan2(norm(cross(groundtruth(:, i), estimate(:, i))), dot(groundtruth(:, i), estimate(:, i))));
+        error(i) = abs(atan2(norm(cross(groundtruth(:, i), estimate(:, i))), dot(groundtruth(:, i), estimate(:, i))));
     end
+    
+    error = reshape(error, [], 1);
+    error = error / max(error);
+    error = ColVectorToImage(error, m, n);
 end
 
