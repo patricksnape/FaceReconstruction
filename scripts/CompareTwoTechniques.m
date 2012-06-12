7f3e20429389f5c42b55a88cc9579489b6079bfd
Patrick Snape <pts08@ic.ac.uk>
2012-06-12 14:46:28 +0100

    edited Compare script

--- scripts/CompareTwoTechniques.m
+++ scripts/CompareTwoTechniques.m
@@ -1,12 +1,13 @@
 % load('data/trainingset')
 % load('data/pga');
-load('data/lights');
+% load('data/lights');
 light1(:, 1) = lights(:,1) / colnorm(lights(:,1));
 light1(:, 2) = lights(:,2) / colnorm(lights(:,2));
 light1(:, 3) = lights(:,3) / colnorm(lights(:,3));
 light1(:, 4) = lights(:,4) / colnorm(lights(:,4));
-% light = [0;0;1];
-texture = rgb2gray(mutaligned);
+light1 = [0;0;1];
+% texture = rgb2gray(blnaligned1);
+texture = I_model{1}.alignedTexture;
 close all;
 % GeneratePCAOnTrainingSet
 % GenerateAEPPCAOnTrainingSet
@@ -15,7 +16,7 @@ close all;
 % clf;
 % [error b n] = SmithGSS(texture, Un, XNavg, light');
 % TexturizeRecoveredFace(texture, n);
-
+% 
 % figure;
 % clf;
 % [b naep] = SmithAEPGSS(texture, UAEPn, XNavg, light');
@@ -23,23 +24,23 @@ close all;
 
 figure;
 clf;
-[ a c ] = NewReconstruction(texture,Un, Ut, XTavg, XNavg, light1);
+[ a c ] = NewReconstructionOld(texture,Un, Ut, XTavg, XNavg, light1);
 [nestimate testimate] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, c, a);
 TexturizeRecoveredFace(texture, nestimate);
 figure;
 imshow(testimate);
 
-figure;
-clf;
-textures(:, :, 1) = rgb2gray(mutaligned1);
-textures(:, :, 2) = rgb2gray(mutaligned2);
-textures(:, :, 2) = rgb2gray(mutaligned3);
-textures(:, :, 2) = rgb2gray(mutaligned4);
-[ amulti cmulti ] = NewReconstructionMulti(textures, Un, Ut, XTavg, XNavg, light1);
-[nestimatemulti testimatemulti] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, cmulti, amulti);
-TexturizeRecoveredFace(texture, testimatemulti);
-figure;
-imshow(testimatemulti);
+% figure;
+% clf;
+% textures(:, :, 1) = rgb2gray(mutaligned1);
+% textures(:, :, 2) = rgb2gray(mutaligned2);
+% textures(:, :, 2) = rgb2gray(mutaligned3);
+% textures(:, :, 2) = rgb2gray(mutaligned4);
+% [ amulti cmulti ] = NewReconstructionMulti(textures, Un, Ut, XTavg, XNavg, light1);
+% [nestimatemulti testimatemulti] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, cmulti, amulti);
+% TexturizeRecoveredFace(texture, nestimatemulti);
+% figure;
+% imshow(testimatemulti);
 
 % figure;
 % clf;
@@ -53,4 +54,4 @@ imshow(testimatemulti);
 % smithError = AngularError(I_model{197}.alignedNormals, n)
 % AEPError = AngularError(I_model{197}.alignedNormals, naep)
 % PGAError = AngularError(I_model{197}.alignedNormals, npga)
-% myError = AngularError(I_model{197}.alignedNormals, nestimate)
+myError = AngularError(I_model{1}.alignedNormals, nestimate)
