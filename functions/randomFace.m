b0f902d9e8672a48e83f37e0c86866234cdd368b
patricksnape <pts08@ic.ac.uk>
2012-10-22 15:09:00 +0100

    Choose a random face instead of the first one

--- functions/randomFace.m
+++ functions/randomFace.m
@@ -13,8 +13,8 @@ alphaArray = model_size * (alphas2 ./ repmat(sum(abs(alphas2), 1), 199, 1));
 betaArray = model_size * (betas2 ./ repmat(sum(abs(betas2), 1), 199, 1));
 
 shape = model.shapeMean * ones([1 199]) + model.shapePC(:,1:199) * (alphaArray .* (model.shapeEV(1:199) * ones([1 199])));
-shape = shape(:,1);
+shape = shape(:,randi([1 199],1,1));
 texture = model.textureMean * ones([1 199]) + model.texturePC(:,1:199) * (betaArray .* (model.textureEV(1:199) * ones([1 199])));
-texture = texture(:,1);
+texture = texture(:,randi([1 199],1,1));
 end
 
