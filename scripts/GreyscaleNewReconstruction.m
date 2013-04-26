0d87bffacbf2fd3a34ac44d824b07e41d0b58f18
patricksnape <pts08@ic.ac.uk>
2013-04-26 14:52:39 +0100

    Fixed wrong parameters

--- scripts/GreyscaleNewReconstruction.m
+++ scripts/GreyscaleNewReconstruction.m
@@ -4,6 +4,6 @@ light = lights(:,1) / colnorm(lights(:,1));
 AlignTrainingSet
 AlignMut1
 GeneratePCAOnTrainingSet
-[ a c ] = NewReconstruction(rgb2gray(mutaligned),Un, Ut, light);
+[ a c ] = NewReconstruction(rgb2gray(mutaligned),Un, Ut, XTavg, XNavg, light);
 [nestimate testimate] = ReconstructFromWeights(XNavg, XTavg, Un, Ut, c, a);
-TexturizeRecoveredFace(testimate, nestimate, ones(size(nestimate)))
+TexturizeRecoveredFace(testimate, nestimate)
