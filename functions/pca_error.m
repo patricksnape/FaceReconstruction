6bacf1013e97139d098c72046607c7978b094bba
patricksnape <patricksnape@gmail.com>
2013-05-15 15:45:55 +0100

    Add ability to calculate error without avg

--- functions/pca_error.m
+++ functions/pca_error.m
@@ -2,6 +2,10 @@ function err = pca_error( X, Xavg )
 %PCA_ERROR Calculates the normalized error of computing the principal
 %components
 
+if (~exist('Xavg', 'var'))
+    Xavg = zeros(size(X, 1), 1);
+end
+
 normalized_normals = matsubcolvec(X, Xavg);
 
 U = myGPCA(normalized_normals, size(X, 2) - 1, 0);
