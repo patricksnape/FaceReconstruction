987b7e8c426fc047841d3baf89aa1b144b56c771
patricksnape <patricksnape@gmail.com>
2013-05-15 15:46:25 +0100

    Change expected input Also add commented out version for manual gradient descent optimisation as described in Smith's paper

--- functions/sphericalmedian.m
+++ functions/sphericalmedian.m
@@ -2,16 +2,26 @@ function [delta_mu] = sphericalmedian(guess, normals)
 %SPHERICALMEAN Summary of this function goes here
 %   Detailed explanation goes here
 
-% Collect the columns into cells
-vec_normals = reshape(normals, [3 numel(normals)/3]);
-
-N = size(vec_normals, 2);
+% Function body if we are using fminsearch
+K = size(normals, 2);
 delta_mu = 0;
 
-for i = 1:N
-    l = logmap(guess, vec_normals(:, i));
+for i = 1:K
+    l = logmap(guess, normals(:, i));
     delta_mu = delta_mu + norm(l);
 end
-delta_mu = delta_mu / N;
 
-end
+% K = size(normals, 2);
+% delta_mu = [0;0;0];
+% prev_mu = [0;0;0];
+% mu = guess;
+% 
+% while norm(prev_mu - mu) > 10^-3
+%     prev_mu = mu;
+%     for i = 1:K
+%         l = logmap(prev_mu, normals(:, i));
+%         delta_mu = delta_mu + l;
+%     end
+%     delta_mu = delta_mu / K;
+%     mu = expmap(prev_mu, delta_mu);
+% end
