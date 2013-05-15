61e9f320e4d721728605dcb03e2f572997ec70db
patricksnape <patricksnape@gmail.com>
2013-05-15 15:42:43 +0100

    Add ability to view arbitrary sized normals

--- functions/ViewNormals.m
+++ functions/ViewNormals.m
@@ -1,20 +1,25 @@
-function ViewNormals(nimg)
+function ViewNormals(nimg, surface)
     if (ndims(nimg) == 2)
         if (size(nimg, 1) == 3)
             nimg = reshape(nimg, [], 1);
         end
-        
+
         nimg = ColVectorToImage3(nimg, 170, 150);
     end
     
-    [x,y] = meshgrid(1:150, 1:170);
-    z = ones(170,150);
-    %nimg = ColVectorToImage3(reshape(C, [], 1), 170, 150);
-
+    if (nargin < 2)
+        [x, y] = meshgrid(1:150, 1:170);
+        z = ones(170, 150);
+    else
+        x = surface(:, :, 1);
+        y = surface(:, :, 2);
+        z = -surface(:, :, 3);
+    end
+    
     q = 5;
-    figure;
+    figure(1);
     quiver3(x(1:q:end,1:q:end),y(1:q:end,1:q:end),z(1:q:end,1:q:end),nimg(1:q:end,1:q:end,1), nimg(1:q:end,1:q:end,2),nimg(1:q:end,1:q:end,3));
 
-    figure;
+    figure(2);
     imshow(nimg, []);
 end
