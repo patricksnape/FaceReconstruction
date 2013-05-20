b6a5d7e243e0679b0c50cc7f943b6af40c7dc16d
patricksnape <patricksnape@gmail.com>
2013-05-20 09:48:46 +0100

    Add new versions of logmap/expmap

--- functions/expmap.m
+++ functions/expmap.m
@@ -1,7 +1,34 @@
-function n = expmap(b, v)
+function n = expmap(b, v, varargin)
 %EXPMAP Exponential map function
 %   Detailed explanation goes here
 
+% Use the inputParser class to validate input arguments.
+inp = inputParser;
+
+% Default to sphere projection
+validProjection = {'stereographic','sphere'};
+inp.addOptional('Projection', validProjection{2}, @(x)any(strcmpi(x,validProjection)));
+
+inp.parse(varargin{:});
+arg = inp.Results;
+clear('inp');
+
+% Early out if v is 0
+if norm(v) == 0
+    n = v;
+    return
+end
+
+% Else perform the chosen projection
+switch arg.Projection
+    case validProjection{1} % Stereographic
+        n = stereographic(b, v);
+    case validProjection{2} % Sphere projection
+        n = sphere_projection(b, v);
+
+end
+
+function n = stereographic(b, v)
 theta = norm(b - v);
 
 % dist(-b,x)
@@ -15,4 +42,7 @@ vprime = b + (((v - b) * 2 * tan(alpha)) / norm(v - b));
 
 n = dist_nbx * ((vprime + b) / norm(vprime + b)) - b;
 
-end
+function n = sphere_projection(b, v)
+vnorm = norm(v);
+
+n = cos(vnorm) .* b + sin(vnorm) .* (v / vnorm);
--- functions/logmap.m
+++ functions/logmap.m
@@ -1,19 +1,42 @@
-function v = logmap(b, x)
+function v = logmap(b, q, varargin)
 %LOGMAP Returns a vector from n1 to n2 on projected on to the tangent plane
 % at n1
 %   Expects unit column vectors
 
+% Use the inputParser class to validate input arguments.
+inp = inputParser;
+
+% Default to sphere projection
+validProjection = {'stereographic', 'sphere'};
+inp.addOptional('Projection', validProjection{2}, @(x) any(strcmpi(x, validProjection)));
+
+inp.parse(varargin{:});
+arg = inp.Results;
+clear('inp');
+
+switch arg.Projection
+    case validProjection{1} % Stereographic
+        v = stereographic(b, q);
+    case validProjection{2} % Sphere projection
+        v = sphere_projection(b, q);
+
+end
+
+function v = stereographic(b, q)
 % dist(-b,x)
-dist_nbx = norm(b + x);
+dist_nbx = norm(b + q);
 % dist(b,x)
-dist_bx = norm(x - b);
+dist_bx = norm(q - b);
 
 alpha = acos((4 + (dist_nbx ^ 2) - (dist_bx ^ 2)) / (4 * dist_nbx));
 
-vprime = ((2 * (b + x)) / (dist_nbx * cos(alpha))) - b;
+vprime = ((2 * (b + q)) / (dist_nbx * cos(alpha))) - b;
 
-theta = acos(dot(b,x));
+theta = acos(dot(b,q));
 
 v = b + ((theta .* (vprime - b)) / norm(vprime - b));
 
-end
+function v = sphere_projection(b, q)
+theta = dot(b, q);
+
+v = (theta / sin(theta)) * (q - b * cos(theta));
