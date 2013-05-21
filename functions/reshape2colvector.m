ec091cf8a1a7993e291550a1c9cbde6cb6bf3901
patricksnape <patricksnape@gmail.com>
2013-05-21 14:48:45 +0100

    Ability to have variable column length

--- functions/reshape2colvector.m
+++ functions/reshape2colvector.m
@@ -1,8 +1,12 @@
-function [ vecs ] = reshape2colvector( mat )
+function [ vecs ] = reshape2colvector(mat, col_length)
 %RESHAPE2COLVECTOR Summary of this function goes here
 %   Detailed explanation goes here
 
-vecs = double(reshape(mat, 3, []));
+if ~exist('col_length', 'var')
+    col_length = 3;
+end
+
+vecs = double(reshape(mat, col_length, []));
 
 end
 
